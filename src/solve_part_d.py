b022867ce93528eb8a856d7b52160d26c0518859
Thomas Breitburd <thomasbreitburd@Thomass-MacBook-Air-77.local>
2023-11-21 12:32:56 +0000

    wrote the solve part d code

---
+++ src/solve_part_d.py
@@ -0,0 +1,15 @@
+from funcs.funcs import signal, background, pdf, plot_pdf_d
+import matplotlib.pyplot as plt
+
+# Define true parameters
+mu_true = 5.28
+sigma_true = 0.018
+lam_true = 0.5
+f_true = 0.1
+
+# Define limits
+alpha = 5
+beta = 5.6
+
+plot_pdf_d(mu_true, sigma_true, lam_true, f_true, alpha, beta)
+plt.savefig("plots/pdf_d.pdf")
