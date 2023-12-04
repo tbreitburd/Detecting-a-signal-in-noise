e16dd05a4b0892764723c3eb75bdab03dfa7ab67
Thomas Breitburd <thomasbreitburd@Thomass-MacBook-Air-77.local>
2023-12-04 12:11:47 +0000

    Changed part c to generate trial parameters randomly and calculate the integral for those

--- src/solve_part_c.py
+++ src/solve_part_c.py
@@ -1,22 +1,19 @@
-# Code these expressions for the probability density in python, in any way you so
-# desire. Plugging in some different values for the parameters 𝜽, use a numerical
-# integration to convince yourself that the total probability density integrates to unity
-# in the range 𝑀 ∈ [5, 5.6]. [2]
-# "
-# You may use predefined methods in libraries such as scipy.stats and
-# scipy.integrate to help you. 
-
 import scipy.integrate as integrate
+import numpy as np
 from funcs import pdf_norm
 
-# Define parameters
-#mu = [5, 5.3, 5.6]; sigma = [0.02, 0.01, 0.03]; lam = [0.5, 0.6, 0.4]; f = [0.9, 0.5, 0.1]; alpha = 5; beta = 5.6
+# Set the number of tests we want to run to test
+num_tests = 3
 
-Int_1,_ = integrate.fixed_quad(lambda x: pdf_norm(x, 5, 0.02, 0.5, 0.9, 5, 5.6), 5, 5.6, n=10)
-Int_2,_ = integrate.fixed_quad(lambda x: pdf_norm(x, 5.3, 0.01, 0.6, 0.5, 5, 5.6), 5, 5.6, n=10)
-Int_3,_ = integrate.fixed_quad(lambda x: pdf_norm(x, 5.6, 0.03, 0.4, 0.1, 5, 5.6), 5, 5.6, n=10)
+print("The integral of the pdf, for the given parameters is:")
 
-print("The integral of the pdf, for the given parameters is: \n" +
-      "\u03BC = 5, \u03C3 = 0.02, \u03BB = 0.5, f = 0.9: " + str(Int_1) + "\n" +
-      "\u03BC = 5.3, \u03C3 = 0.01, \u03BB = 0.6, f = 0.5: " + str(Int_2) + "\n" +
-      "\u03BC = 5.6, \u03C3 = 0.03, \u03BB = 0.4, f = 0.1: " + str(Int_3))
+# Loop over the number of tests
+for i in range(num_tests):
+      mu = np.random.uniform(5, 5.6) # Draw a random value for each parameter from a uniform distribution
+      sigma = np.random.uniform(0.01, 0.03) 
+      lam = np.random.uniform(0.3, 0.7)
+      f = np.random.uniform(0.1, 0.9)
+
+      Integral,_ = integrate.fixed_quad(lambda x: pdf_norm(x, mu, sigma, lam, f, 5, 5.6), 5, 5.6, n = 10)
+      print("\u03BC = {:.4f}, \u03C3 = {:.4f}, \u03BB = {:.4f}, f = {:.4f}: {:.4f}".format(mu, sigma, lam, f, Integral))
+
