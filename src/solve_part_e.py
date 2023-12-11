c019a9cb5f99ec1f00b33ac6f5fdaaa3a409bd98
Thomas Breitburd <tmb76@cam.ac.uk>
2023-12-11 18:27:58 +0100

    Dropped the unbinned minimisation lines, just kept the binned one

--- src/solve_part_e.py
+++ src/solve_part_e.py
@@ -6,67 +6,6 @@ import os
 import matplotlib.pyplot as plt
 
 
-# Set the seed
-np.random.seed(75016)
-
-# Define true parameters
-mu_true = 5.28
-sigma_true = 0.018
-lam_true = 0.5
-f_true = 0.1
-
-# Define limits
-alpha = 5
-beta = 5.6
-
-# Define the pdf
-pdf_true = lambda x: pdf_norm_efg(x, mu_true, sigma_true, lam_true, f_true)
-
-# Generate the sample from this pdf
-M_pdf = accept_reject(pdf_true, alpha, beta, 100000)
-
-# We want to estimate the parameters of the pdf from our generated sample. 
-# First, we use an unbinned negative log likelihood method to estimate the parameters.
-
-# Define the negative log likelihood function
-nll = cost.UnbinnedNLL(M_pdf, pdf_norm_efg)
-
-mi_unbin = Minuit(nll,  f = 0.15,  lam=0.4, mu=5.3, sigma = 0.020)
-mi_unbin.limits['f'] = (0,1)
-mi_unbin.limits['lam'] = (0.01,1)
-mi_unbin.limits['sigma'] = (0,0.05)
-mi_unbin.limits['mu'] = (5,5.6)
-mi_unbin.migrad()
-
-print(mi_unbin)
-
-# Here, we use a binned negative log likelihood method to estimate the parameters.
-# So, we want to minimize that negative log likelihood function.
-
-# First, we bin the data to get the counts in each bin, and the bin edges.
-bins = np.linspace(5,5.6,1000)
-bin_counts, bin_edges = np.histogram(M_pdf, bins=bins)
-
-# Now, we define the negative log likelihood function which we will minimize using iminuit.
-# This function takes in the bin counts, bin edges, and the cdf of the pdf we want to fit to.
-binned_nll = cost.BinnedNLL(bin_counts, bin_edges, cdf_efg)
-
-# Now, we minimize that function using iminuit.
-mi_bin = Minuit(binned_nll,  f = 0.2,  lam=0.4, mu=5.3, sigma = 0.02)
-mi_bin.limits['f'] = (0,0.5)
-mi_bin.limits['lam'] = (0.01,1)
-mi_bin.limits['sigma'] = (0,0.05)
-mi_bin.limits['mu'] = (5,5.6)
-mi_bin.migrad()
-
-print(mi_bin)
-
-# Get returned parameter estimates
-mu_est = mi_bin.values['mu']
-sigma_est = mi_bin.values['sigma']
-lam_est = mi_bin.values['lam']
-f_est = mi_bin.values['f']
-
 # Define the plotting function
 def plot_e(pdf, gen_sample, mu_hat, sigma_hat, lam_hat, f_hat, alpha, beta):
     """
@@ -126,7 +65,7 @@ def plot_e(pdf, gen_sample, mu_hat, sigma_hat, lam_hat, f_hat, alpha, beta):
     
     # Top figure true pdf, background and signal lines
     x = np.linspace(bin_widths[0], bin_widths[-1], 200)
-    ax[0,0].plot(x, f_hat * signal_ * N * bin_width[0],'--',color = 'r', label='f * Signal, s(M; \u03BC, \u03C3)')
+    ax[0,0].plot(x, f_hat * signal_ * N * bin_width[0],'--',color = 'r', label='f x Signal, s(M; \u03BC, \u03C3)')
     ax[0,0].plot(x, (1 - f_hat) * background_ * N * bin_width[0],'--', color = 'g', label='Background, b(M; \u03BB)')
     ax[0,0].plot(x, pdf_vals * N * bin_width[0],'--', color = 'k', label='PDF')
     ax[0,0].legend(loc='upper right')
@@ -163,6 +102,54 @@ def plot_e(pdf, gen_sample, mu_hat, sigma_hat, lam_hat, f_hat, alpha, beta):
     plt.show()
 
     return None
+
+
+# Set the seed
+np.random.seed(75016)
+
+# Define true parameters
+mu_true = 5.28
+sigma_true = 0.018
+lam_true = 0.5
+f_true = 0.1
+
+# Define limits
+alpha = 5
+beta = 5.6
+
+# Define the pdf
+pdf_true = lambda x: pdf_norm_efg(x, mu_true, sigma_true, lam_true, f_true)
+
+# Generate the sample from this pdf
+M_pdf = accept_reject(pdf_true, alpha, beta, 100000)
+
+# Here, we use a binned negative log likelihood method to estimate the parameters.
+# So, we want to minimize that negative log likelihood function.
+
+# First, we bin the data to get the counts in each bin, and the bin edges.
+bins = np.linspace(5,5.6,1000)
+bin_counts, bin_edges = np.histogram(M_pdf, bins=bins)
+
+# Now, we define the negative log likelihood function which we will minimize using iminuit.
+# This function takes in the bin counts, bin edges, and the cdf of the pdf we want to fit to.
+binned_nll = cost.BinnedNLL(bin_counts, bin_edges, cdf_efg)
+
+# Now, we minimize that function using iminuit.
+mi_bin = Minuit(binned_nll,  f = 0.2,  lam=0.4, mu=5.3, sigma = 0.02)
+mi_bin.limits['f'] = (0,0.5)
+mi_bin.limits['lam'] = (0.01,1)
+mi_bin.limits['sigma'] = (0,0.05)
+mi_bin.limits['mu'] = (5,5.6)
+mi_bin.migrad()
+mi_bin.hesse()
+
+print(mi_bin)
+
+# Get returned parameter estimates
+mu_est = mi_bin.values['mu']
+sigma_est = mi_bin.values['sigma']
+lam_est = mi_bin.values['lam']
+f_est = mi_bin.values['f']
 
 
 # Plot the pdf with the estimated parameters
