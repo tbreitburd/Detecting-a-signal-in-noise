0883c378741f917330527d126d5d1c63bf356591
Thomas Breitburd <tmb76@cam.ac.uk>
2023-12-16 23:01:15 +0100

    Chnaged the plotting functions to be a bit more high quality

--- src/solve_part_d.py
+++ src/solve_part_d.py
@@ -13,55 +13,7 @@ f_true = 0.1
 alpha = 5
 beta = 5.6
 
-# Define the plotting functions
-
-def plot_pdf_d(pdf, mu, sigma, lam, f, alpha, beta):
-    """
-    This function plots the signal component,
-    the background component,
-    the pdf of M, using the normalisation factor derived in (b),
-    all overlaid on the same plot.
-    ----------------------------
-    Inputs:
-    alpha: lower limit of M, float
-    beta: upper limit of M, float
-    mu: mean of signal (normal) component, float, must be within alpha and beta
-    sigma: width of signal (normal) component, float, must be positive
-    lam: decay parameter of background (exponential) component, float
-    f: ratio of signal/background components, float
-    ----------------------------
-    Outputs:
-    plot of signal component, background component, normalised pdf of M
-    """
-
-    # Define x-axis
-    x = np.linspace(alpha, beta, 1000)
-
-    # Define signal component
-    signal_ = signal(x, mu, sigma)
-
-    # Define background component
-    background_ = background(x, lam)
-
-    # Define pdf
-    pdf_true = pdf(x, mu, sigma, lam, f, alpha, beta)
-
-    # Plot
-    plt.plot(x, signal_,'--',color = 'r', label='Signal, s(M; \u03BC, \u03C3)')
-    plt.plot(x, background_,'--', color = 'g', label='Background, b(M; \u03BB)')
-    plt.plot(x, pdf_true, color = 'k', label='PDF')
-    plt.xlabel('M')
-    plt.ylabel('Probability density')
-    plt.legend()
-    proj_dir = os.getcwd()
-    plots_dir = os.path.join(proj_dir, 'plots')
-    os.makedirs(plots_dir, exist_ok=True)
-    plot_dir = os.path.join(plots_dir, 'plot_pdf_d.png')
-    plt.savefig(plot_dir)
-    plt.show()
-
-    return None
-
+# Define the plotting function
 
 def plot_pdf_d_comp(pdf, mu, sigma, lam, f, alpha, beta):
     """
@@ -96,11 +48,13 @@ def plot_pdf_d_comp(pdf, mu, sigma, lam, f, alpha, beta):
     pdf_true = pdf(x, mu, sigma, lam, f, alpha, beta)
 
     # Plot
-    plt.plot(x, signal_,'--',color = 'r', label='Signal, s(M; \u03BC, \u03C3)')
-    plt.plot(x, background_,'--', color = 'g', label='Background, b(M; \u03BB)')
+    plt.plot(x, f * signal_,'--',color = 'r', label='f x Signal, s(M; \u03BC, \u03C3)')
+    plt.plot(x, (1-f)*background_,'--', color = 'g', label='(1-f) x Background, b(M; \u03BB)')
     plt.plot(x, pdf_true, color = 'k', label='PDF')
     plt.xlabel('M')
     plt.ylabel('Probability density')
+    plt.title('Plot of signal component, background component, \n and normalised p.d.f. of M')
+    plt.grid()
     plt.legend()
     proj_dir = os.getcwd()
     plots_dir = os.path.join(proj_dir, 'plots')
